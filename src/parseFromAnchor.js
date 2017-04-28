1243a2c15aff431e213a8648143e4e69218a2da6
Fernando Souza <nandosouzafilho@gmail.com>
2017-04-28 00:00:18 -0300

    Avoids default port number 0 in Safari 5

--- src/parseFromAnchor.js
+++ src/parseFromAnchor.js
@@ -1,6 +1,19 @@
 'use strict';
 
 /**
+ * Extracts the port for a given uri. This function helps to support for
+ * Safari 5, that returns '0' when the port is not present in the URI string.
+ * @param {*=} opt_uri Optional string URI to parse
+ */
+function extractPortFromURI(uri) {
+	let port = parseInt(uri.split(':').pop());
+	if (isNaN(port)) {
+		return '';
+	}
+	return port.toString()
+}
+
+/**
  * Parses the given uri string into an object.
  * @param {*=} opt_uri Optional string URI to parse
  */
@@ -12,7 +25,7 @@ function parseFromAnchor(opt_uri) {
 		hostname: link.hostname,
 		password: link.password,
 		pathname: link.pathname[0] === '/' ? link.pathname : '/' + link.pathname,
-		port: link.port,
+		port: extractPortFromURI(opt_uri),
 		protocol: link.protocol,
 		search: link.search,
 		username: link.username
